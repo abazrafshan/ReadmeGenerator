f7c9539d0331408e0bf0c4c5112fdb76a548fa35
Arash Bazrafshan <abazrafshan123@gmail.com>
2020-05-16 15:08:41 -0700

    added content to WriteToFile and Init functions

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,8 @@
-// Prompt user with list of questions through inquirer 
-// and save each response
-// responses will be used in GenerateMarkdown where
-// template literal will be used to populate Readme
-// based on user feedback to each question
-// WritetoFile function will take generateMarkdown content
-// and write this file to a named readme.md
-
 const fs = require("fs");
 const inquirer = require("inquirer");
+const util = require("util");
+var generateMarkdown = require("./utils/generateMarkdown");
+const writeFileAsync = util.promisify(fs.writeFile);
 
 const questions = [
 "What is the your Github username?",
@@ -21,72 +16,76 @@ const questions = [
 "What does the user need to know about using the repo?",
 "What does the user need to know about contributing to the repo?"
 ];
-
-inquirer.prompt([
-    {
-        type: "input",
-        message: questions[0],
-        name: "username"
-    },
-    {
-        type: "input",
-        message: questions[1],
-        name: "email"
-    },
-    {
-        type: "input",
-        message: questions[2],
-        name: "projectURL"
-    },
-    {
-        type: "input",
-        message: questions[3],
-        name: "projectName"
-    },
-    {
-        type: "input",
-        message: questions[4],
-        name: "description"
-    },
-    {
-        type: "list",
-        message: questions[5],
-        name: "license",
-        choices: [
-            "MIT",
-            "APACHE 2.0",
-            "GPL 3.0",
-            "BSD 3",
-            "None"
-        ]
-    },
-    {
-        type: "input",
-        message: questions[6],
-        name: "install"
-    },
-    {
-        type: "input",
-        message: questions[7],
-        name: "test"
-    },
-    {
-        type: "input",
-        message: questions[8],
-        name: "info"
-    },
-    {
-        type: "input",
-        message: questions[9],
-        name: "contribute"
-    },
-]).then(writeToFile("readme.md", data));
-
-function writeToFile(fileName, data) {
+function promptUser(){
+    return inquirer.prompt([
+        {
+            type: "input",
+            message: questions[0],
+            name: "username"
+        },
+        {
+            type: "input",
+            message: questions[1],
+            name: "email"
+        },
+        {
+            type: "input",
+            message: questions[2],
+            name: "projectURL"
+        },
+        {
+            type: "input",
+            message: questions[3],
+            name: "projectName"
+        },
+        {
+            type: "input",
+            message: questions[4],
+            name: "description"
+        },
+        {
+            type: "list",
+            message: questions[5],
+            name: "license",
+            choices: [
+                "MIT",
+                "APACHE 2.0",
+                "GPL 3.0",
+                "BSD 3",
+                "None"
+            ]
+        },
+        {
+            type: "input",
+            message: questions[6],
+            name: "install"
+        },
+        {
+            type: "input",
+            message: questions[7],
+            name: "test"
+        },
+        {
+            type: "input",
+            message: questions[8],
+            name: "info"
+        },
+        {
+            type: "input",
+            message: questions[9],
+            name: "contribute"
+        }
+    ]);
 }
 
-function init() {
+function writeToFile(fileName, data) {
+    const content = generateMarkdown(data);
+    return writeFileAsync(fileName, content);
+}
 
+
+function init() {
+    promptUser().then(writeToFile("readme.md",data));
 }
 
 init();
