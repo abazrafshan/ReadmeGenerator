a96056afa8f6314b8e0c0ed305974205cc9baecd
Arash Bazrafshan <abazrafshan123@gmail.com>
2020-05-16 14:01:29 -0700

    Added inquirer prompt to index

--- Develop/index.js
+++ Develop/index.js
@@ -23,6 +23,65 @@ const questions = [
 "What does the user need to know about contributing to the repo?"
 ];
 
+inquirer.prompt([
+    {
+        type: "input",
+        message: questions[0],
+        name: "username"
+    },
+    {
+        type: "input",
+        message: questions[1],
+        name: "email"
+    },
+    {
+        type: "input",
+        message: questions[2],
+        name: "projectURL"
+    },
+    {
+        type: "input",
+        message: questions[3],
+        name: "projectName"
+    },
+    {
+        type: "input",
+        message: questions[4],
+        name: "description"
+    },
+    {
+        type: "list",
+        message: questions[5],
+        name: "license",
+        choices: [
+            "MIT",
+            "APACHE 2.0",
+            "GPL 3.0",
+            "BSD 3",
+            "None"
+        ]
+    },
+    {
+        type: "input",
+        message: questions[6],
+        name: "install"
+    },
+    {
+        type: "input",
+        message: questions[7],
+        name: "test"
+    },
+    {
+        type: "input",
+        message: questions[8],
+        name: "info"
+    },
+    {
+        type: "input",
+        message: questions[9],
+        name: "contribute"
+    },
+])
 function writeToFile(fileName, data) {
 }
 
